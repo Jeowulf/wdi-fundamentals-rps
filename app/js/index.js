e6fd91ec314a373359df76ee314b54eb8ed4cad5
Justin Jacot <justinowenjacot@icloud.com>
2015-04-09 21:01:17 -0400

    added all the conditionals concerning victory outcomes in the rockpaperscissors program

--- app/js/index.js
+++ app/js/index.js
@@ -26,7 +26,7 @@ function getPlayerMove(move) {
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
     move = move || getInput(); 
-    return move;
+    return move
     }
 
 
@@ -35,7 +35,7 @@ function getComputerMove(move) {
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     move = move || randomPlay();
-    return move;
+    return move  
     }
 
 
@@ -46,37 +46,38 @@ function getWinner(playerMove,computerMove) {
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     if (playerMove === computerMove) {
         winner = 'tie';
-        console.log("Lame.  Freaking tie.");
+        console.log("Lame.  Freaking tie.")
     return winner;
-} else if (playerMove === 'rock' && computerMove === 'paper') {
-        winner = 'computer';
-        console.log("Computer whooped ya. :/");
+} else if (playerMove === 'rock');
+       if (computerMove === 'paper'); {
+        winner = 'computer'
+        console.log("Computer whooped ya. :/")
         return winner;
-} else if (computerMove === 'rock' && playerMove === 'paper') {
-       winner = 'player';
-       console.log("You took that durn robot down");
-       return winner;
-} else if (playerMove === 'paper' && computerMove === 'scissors') {
-        winner = 'computer';
-        console.log("Well dangit.");
-        return winner;
-} else if (computerMove === 'paper' && playerMove === 'scissors') {
-        winner = 'player';
-        console.log("HAHA.  Rock paper scissors is no place for mercy.");
-        return winner;
-} else if (playerMove === 'scissors' && computerMove === 'rock') {
-        winner = 'computer';
-        console.log("The machines are taking over.  Fight harder or it's judgement day");
-        return winner;
-} else if (playerMove === 'rock' && computerMove === 'scissors') {
-        winner = 'player';
-        console.log("Send the computer home crying");
-        return winner;
+} else if (computerMove === 'rock');
+       if (playerMove === 'paper'); {
+       winner = 'player'
+       console.log("You took that durn robot down")
+       return winner
+       } else if (playerMove === 'paper');
+       if (computerMove === 'scissors'); {
+        winner = 'computer'
+        console.log("Well dangit.")
+        return winner
+       } else if (computerMove === 'paper');
+       if (playerMove === 'scissors'); {
+        winner = 'player'
+        console.log("HAHA.  Rock paper scissors is no place for mercy.")
+        return winner
+       } else if (playerMove === 'scissors');
+       if (computerMove === 'rock'); {
+        winner = 'computer'
+        console.log("The machines are taking over.  Fight harder or it's judgement day")
+        return winner
        } else {
-        console.log("WTF is going on?!?!?")
+        winner = 'player'
+        console.log("Send the computer home crying")
        }
 
-}
        
 
 function playToFive() {
